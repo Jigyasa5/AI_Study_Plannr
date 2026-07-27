f8b33a3a9d1e2bece4cda2e94f4533785ac99e52
Jigyasa <jigyasvermadelhi@gmail.com>
2026-07-27 16:04:16 +0530

    Phase 3 : Implemented user login page

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "studyplanner"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+# ---------------- SIGNUP ----------------
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -18,16 +21,66 @@ def signup():
         cursor = conn.cursor()
 
         cursor.execute(
-            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            "INSERT INTO users(name,email,password) VALUES(?,?,?)",
             (name, email, password)
         )
 
         conn.commit()
         conn.close()
 
-        return "User Registered Successfully!"
+        return redirect("/login")
 
     return render_template("signup.html")
+
+
+# ---------------- LOGIN ----------------
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE email=? AND password=?",
+            (email, password)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            session["user"] = user[1]
+            return redirect("/dashboard")
+
+        return "Invalid Email or Password"
+
+    return render_template("login.html")
+
+
+# ---------------- DASHBOARD ----------------
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    return render_template("dashboard.html", name=session["user"])
+
+
+# ---------------- LOGOUT ----------------
+@app.route("/logout")
+def logout():
+
+    session.pop("user", None)
+
+    return redirect("/login")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
